4e8e412d6b73d076eacc618778495885ec1ff672
ElianaRosero <66647612+ElianaRosero@users.noreply.github.com>
2020-07-26 23:26:20 -0500

    Updates

---
+++ precache-manifest.e9939556bcd8eb076ec230014e7425bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e543e6c24be5f4dc3f7a89954b0c6a7",
+    "url": "/Calculadora/index.html"
+  },
+  {
+    "revision": "575a8cbf3f01939147f5",
+    "url": "/Calculadora/static/css/main.1a434162.chunk.css"
+  },
+  {
+    "revision": "957638ea8982320dc693",
+    "url": "/Calculadora/static/js/2.e3675be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculadora/static/js/2.e3675be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575a8cbf3f01939147f5",
+    "url": "/Calculadora/static/js/main.e7f5d956.chunk.js"
+  },
+  {
+    "revision": "9e583a7ce9ae68b9c40e",
+    "url": "/Calculadora/static/js/runtime-main.adb4f07b.js"
+  }
+]);
